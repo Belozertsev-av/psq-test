ba79e9a31936a921c921a4cc115692b49bc2eb96
Belozertsev-av <andrew.vl.belozertsev@gmail.com>
2024-03-17 18:12:24 +0300

    vercel hotfix

--- api/variants.js
+++ api/variants.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://psq-test.vercel.app:3000"
+const API_URL = "https://psq-test.vercel.app/api"
 
 // Имитация GET запроса
 export function getVariants() {
